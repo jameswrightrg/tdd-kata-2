6e1c2b15327d0ec04d0de3eb65631fb31766a141
James Wright <james.wright@red-gate.com>
2017-03-30 13:28:52 +0100

    [Green] pyramid factory

---
+++ tddkata/PyramidFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace tddkata
+{
+    public class PyramidFactory
+    {
+        private class Pyramid : IShape
+        {
+            public double GetVolume()
+            {
+                return 0;
+            }
+        }
+        public static IShape Create(int i, int i1)
+        {
+            return new Pyramid();
+        }
+    }
+}
